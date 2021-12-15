e88bef35f4c179ea6b209e82696026be780899b5
Mark Bedner <bednermb3420@gmail.com>
2021-12-14 23:22:55 -0500

    added a couple of sections

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ function App() {
     <div className="app">
       <div
         id="noise"
-        class="fixed -top-1/2 -left-1/2 pointer-events-none"
+        class="fixed -top-1/2 -left-1/2 pointer-events-none z-0"
       ></div>
-      <section id="hero" className="overflow-x-hidden">
+      <section id="hero">
         <header className="flex w-3/4 m-auto py-12 gap-12 items-center justify-between z-50 relative">
           <a href="/" id="logo" className="text-white text-3xl">
             Logo
@@ -53,11 +53,9 @@ function App() {
             </ul>
           </nav>
         </header>
-        {/* <section id="hero" className="h-screen 2xl:h-auto py-32"> */}
-        <div className="w-3/4 m-auto flex flex-col py-32">
-          {/* <div className="w-3/4 2xl:w-1/2 m-auto flex justify-between flex-col h-full"> */}
-          <div className="w-1/2">
-            <h1 className="text-white text-7xl mb-6 font-semibold">
+        <div className="w-3/4 m-auto flex flex-col 2xl:py-32 md:py-16">
+          <div className="2xl:w-1/2 md:w-3/5">
+            <h1 className="text-white md:text-7xl mb-6 font-semibold">
               Lorem ipsum dolor sit amet cons ectetur
             </h1>
             <p className="text-white w-5/6 opacity-70 mb-8">
@@ -68,15 +66,9 @@ function App() {
             </p>
             <a
               href="/"
-              className="inline-flex gap-4 text-white items-center mt-4 group"
+              className="inline-flex gap-4 text-white items-center 2xl:mt-4 md:mt-0 group"
             >
-              <div
-                className="w-12 h-12 rounded-full flex items-center justify-center text-xl bg-blue-600 border border-solid border-blue-500 group-hover:bg-transparent transition ease-in-out overflow-hidden relative font-bold"
-                // style={{
-                //   background: "rgb(13, 135, 255)",
-                //   border: "rgb(13, 135, 255)",
-                // }}
-              >
+              <div className="w-12 h-12 rounded-full flex items-center justify-center text-xl bg-blue-600 border border-solid border-blue-500 group-hover:bg-transparent transition ease-in-out overflow-hidden relative font-bold">
                 <FiArrowRight />
               </div>
               <div className="uppercase overflow-hidden h-5 transition ease-in-out">
@@ -91,8 +83,7 @@ function App() {
               </div>
             </a>
           </div>
-          <div className="flex gap-2 w-1/2 mt-40">
-            {/* <div className="flex gap-2 w-1/2 2xl:mt-72"> */}
+          <div className="flex gap-2 w-1/2 2xl:mt-40 md:mt-24">
             <div className="w-1/2">
               <div className="numbers text-white mb-4 tracking-widest text-sm">
                 01
@@ -125,6 +116,56 @@ function App() {
           className="absolute w-1/3 right-32 top-0 pointer-events-none"
           alt="lights"
         />
+      </section>
+
+      <section>
+        <div className="w-3/4 m-auto flex md:py-32 gap-24 justify-center items-center">
+          <div className="w-1/2">
+            <img
+              className="w-full"
+              alt="test"
+              src="https://media.newyorker.com/photos/60d7831254673ca79c73fdf7/master/w_120,c_limit/LewisKraus-UFOreport.jpg"
+            />
+          </div>
+          <div className="w-1/2">
+            <div className="w-5/6">
+              <h2 className="text-5xl font-semibold text-white mb-4">
+                Quis nostrud exercitation ullamco
+              </h2>
+              <p className="text-white w-5/6 opacity-70 mb-8">
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+                dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="w-3/4 m-auto flex md:py-32 gap-24 justify-center items-center">
+          <div className="w-1/2">
+            <div className="ml-20">
+              <h2 className="text-5xl font-semibold text-white mb-4">
+                Quis nostrud exercitation ullamco
+              </h2>
+              <p className="text-white w-5/6 opacity-70 mb-8">
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+                dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur.
+              </p>
+            </div>
+          </div>
+          <div className="w-1/2">
+            <img
+              className="w-full"
+              alt="test"
+              src="https://media.newyorker.com/photos/60d7831254673ca79c73fdf7/master/w_120,c_limit/LewisKraus-UFOreport.jpg"
+            />
+          </div>
+        </div>
       </section>
     </div>
   );
